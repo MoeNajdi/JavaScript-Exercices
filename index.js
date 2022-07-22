3b4ecdf78aba31632ba6c428634620a33370688b
MoeNajdi <mnajdi243@gmai.com>
2022-07-22 15:22:23 +0300

    3rd

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let count = 0
+var count = 0
+let countEl = document.getElementById("count-el")
 function increment(){
 count += 1
-document.getElementById("count-el").innerText = count
+countEl.innerText = count
 }
 
 function decrement(){
     count -= 1
-    document.getElementById("count-el").innerText = count
+    countEl.innerText = count
 }
